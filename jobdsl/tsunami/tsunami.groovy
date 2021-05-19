b0df461933682c79618cb6aee2ef53388fdbf959
Aviel Revivo <aviel.revivo@imperva.com>
2021-05-19 21:21:19 +0300

    Initial commit - seed job

---
+++ jobdsl/tsunami/tsunami.groovy
@@ -0,0 +1,14 @@
+pipelineJob('tsunami') {
+  definition {
+    cpsScm {
+        scm {
+          git {
+            remote {
+              url ('https://github.com/avielr/tsunami-glt.git')
+            }
+          }
+        }
+        scriptPath("Jenkinsfile")
+    }
+  }
+}
