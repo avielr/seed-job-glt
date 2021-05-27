09c1faa41523f63abbeb9d1becd45e247b3664a1
Aviel Revivo <aviel.revivo@imperva.com>
2021-05-27 19:48:32 +0300

    change name

--- jobdsl/tsunami/deploy_tsunami.groovy
+++ jobdsl/tsunami/deploy_tsunami.groovy
@@ -1,4 +1,4 @@
-pipelineJob('2_Deploy_glt_tsunami') {
+pipelineJob('2_Deploy_glt_tsunami_test_params') {
   parameters {
     stringParam('serversToScan', '184.168.131.241,184.168.139.128,185.17.73.13', 'IPs to scan')
     stringParam('EMAIL_ADDRESS', 'aviel1988@gmail.com, demo.avlr@gmail.com', 'Enter EMAIL address to send the report to')
