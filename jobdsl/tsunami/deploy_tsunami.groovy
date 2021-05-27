cb7b63572bcf49dd08330d26b7ccba2b7c24a5ae
Aviel Revivo <aviel.revivo@imperva.com>
2021-05-27 19:48:04 +0300

    add deploy params

--- jobdsl/tsunami/deploy_tsunami.groovy
+++ jobdsl/tsunami/deploy_tsunami.groovy
@@ -1,4 +1,8 @@
 pipelineJob('2_Deploy_glt_tsunami') {
+  parameters {
+    stringParam('serversToScan', '184.168.131.241,184.168.139.128,185.17.73.13', 'IPs to scan')
+    stringParam('EMAIL_ADDRESS', 'aviel1988@gmail.com, demo.avlr@gmail.com', 'Enter EMAIL address to send the report to')
+  }
   definition {
     cpsScm {
         scm {
